a967ae221b91da4d360e8a8f3e1e8f662d516e42
Vatsal varma <144918256+vatsalvarma@users.noreply.github.com>
2026-03-18 12:39:36 +0530

    Remove empty lines from script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
- 
-
 const images = [ "https://images.unsplash.com/photo-1507525428034-b723cf961d3ev", 
                 "https://images.unsplash.com/photo-1493244040629-496f6d136cc3v", 
                 "https://images.unsplash.com/photo-1501785888041-af3ef285b470v",
